93225c64f62c54a86578066c716c28f7f988d351
Feng <fengparra@outlook.com>
2022-07-26 16:22:05 -0400

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (let element of elements)
+        if (array.indexOf(element) != -1) {
+            let elementIndex = array.indexOf(element);
+            array.splice(elementIndex, 1);
+        }
+    return array;
 };
 
 // Do not edit below this line
