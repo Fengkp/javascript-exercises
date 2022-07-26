11771561c14fdec238e0e8cdd1897a4e83b71599
Feng <fengparra@outlook.com>
2022-07-26 16:31:44 -0400

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 >= 0 
+        && num2 >= 0 
+        && typeof num1 === 'number' 
+        && typeof num2 === 'number') {
+        let start = num1;
+        let end = num2;
+        let sum = 0;
+
+        if (num2 < num1) {
+            start = num2;
+            end = num1;
+        }
+
+        for (let i = start; i <= end; i++) 
+            sum += i;
+        return sum;
+    }
+    return 'ERROR'
 };
 
 // Do not edit below this line
