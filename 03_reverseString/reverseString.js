e03c57c306516a4e4481936d2787cdd01309ec69
Feng <fengparra@outlook.com>
2022-07-26 15:28:21 -0400

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = '';
+    const stringLength = string.length;
 
+    for (let i = stringLength - 1; i >= 0; i--) 
+        reverseString = reverseString.concat(string[i]);
+    return reverseString;
+    
 };
 
 // Do not edit below this line
